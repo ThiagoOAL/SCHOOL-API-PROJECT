698f8f3decebe17d6207d0b98cfc389a59e178c3
ThiagoOAL <thiago-leite01@hotmail.com>
2022-03-11 22:36:06 -0300

    criação da entities e da module

--- src/student-tasks/entities/student-task.entity.ts
+++ src/student-tasks/entities/student-task.entity.ts
@@ -1,1 +1,30 @@
-export class StudentTask {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { student, studentSchema} from 'src/students/entities/student.entity';
+import { task} from 'src/tasks/entities/task.entity';
+
+export type StudentTaskDocument = StudentTask & Document;
+
+@Schema()
+export class StudentTask {
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'student' }] })
+  student: student[];
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'task' }] })
+  task: task[];
+
+  @Prop({ required: true })
+  dateTime: Date;
+  
+  @Prop({ required: true })
+  score: number;
+
+  @Prop()
+  text: string;
+
+}
+
+export const StudentTaskSchema = SchemaFactory.createForClass(StudentTask);
+
--- src/student-tasks/student-tasks.module.ts
+++ src/student-tasks/student-tasks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { StudentTasksService } from './student-tasks.service';
 import { StudentTasksController } from './student-tasks.controller';
+import { StudentTask, StudentTaskSchema } from './entities/student-task.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: StudentTask.name, schema: StudentTaskSchema }])],
   controllers: [StudentTasksController],
   providers: [StudentTasksService]
 })
